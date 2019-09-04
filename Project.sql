70b17edd71242a7ed09631abc0a9cf26deadf3f4
Abdullah-Alaql <54897390+Abdullah-Alaql@users.noreply.github.com>
2019-09-04 18:08:14 +0300

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -1,3 +1,6 @@
-SELECT * FROM city_data WHERE  city='Mecca' and country = 'Saudi Arabia';
-
+/* this code is for extracting Mecca data from the data base*/
+SELECT * FROM city_data WHERE  city='Mecca' AND country = 'Saudi Arabia';
+/* this code is for cheeking the avilable Saudi arabia's cities in the data base for local comparing*/
+SELECT * FROM city_list WHERE country='Saudi Arabia';
+/* this code is for extracting global data from the data base*/
 SELECT * FROM city_data ;
