de9819c8c634edf9127ca64c55db3a6e32af130e
Abdullah-Alaql <54897390+Abdullah-Alaql@users.noreply.github.com>
2019-09-04 18:14:17 +0300

    Update and rename Project.sql to extract.sql

--- extract.sql
+++ extract.sql
@@ -1,6 +1,8 @@
 /* this code is for extracting Mecca data from the data base*/
 SELECT * FROM city_data WHERE  city='Mecca' AND country = 'Saudi Arabia';
+
 /* this code is for cheeking the avilable Saudi arabia's cities in the data base for local comparing*/
 SELECT * FROM city_list WHERE country='Saudi Arabia';
+
 /* this code is for extracting global data from the data base*/
-SELECT * FROM city_data ;
+SELECT * FROM global_data;
