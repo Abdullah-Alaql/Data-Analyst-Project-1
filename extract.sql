7ddce2fcd26636b2acc96a0e1b025204e035e4f8
Abdullah-Alaql <54897390+Abdullah-Alaql@users.noreply.github.com>
2019-09-04 18:17:50 +0300

    Update extract.sql
    This is the first step of the project which is extracting the data from the database.

--- extract.sql
+++ extract.sql
@@ -1,13 +1,3 @@
-
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "## UDACITY Data Analysis Nanodegree Program - Project 1 (Exploring Weather Trends);\n",
-    "this is the second step of the project which is about reading the data and visualize it properly by using 'Moving Average';"
-   ]
-  },
 /* this code is for extracting Mecca data from the data base*/
 SELECT * FROM city_data WHERE  city='Mecca' AND country = 'Saudi Arabia';
 
