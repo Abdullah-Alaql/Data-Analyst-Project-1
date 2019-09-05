d101f643cc1c33742ddb646016e18fc6fae241da
Abdullah-Alaql <54897390+Abdullah-Alaql@users.noreply.github.com>
2019-09-05 23:04:52 +0300

    Update DataExtract.sql

--- DataExtract.sql
+++ DataExtract.sql
@@ -5,14 +5,15 @@ this is the first step in the project, belew is  the code that was used for extr
 note: I couldn't find my home town Jeddah, so instead I used Mecca which is the closest city to Jeddah.
 */
 
-
 /* this code is for extracting Mecca data from the data base*/
-SELECT * FROM city_data WHERE  city='Mecca' AND country = 'Saudi Arabia';
+SELECT year,avg_temp
+FROM city_data where city='Mecca';
 
 /* this code is for cheeking the avilable Saudi arabia cities in the data base for local comparing*/
 SELECT * FROM city_list WHERE country='Saudi Arabia';
 /* this code is for extracting Mecca data from the data base*/
-SELECT * FROM city_data WHERE  city='Riyadh' AND country = 'Saudi Arabia';
+SELECT year,avg_temp
+FROM city_data where city='Riyadh';
 
 /* this code is for extracting global data from the data base*/
 SELECT * FROM global_data;
